0244200e6c234ddd1d97f6f8f7b62b0518dcfa89
d-chandrahas <chandrahasreddy732@gmail.com>
2023-02-13 16:57:48 +0530

    changed path of reset_db script

--- database management/reset_db.sql
+++ database management/reset_db.sql
@@ -4,7 +4,7 @@ DROP TABLE IF EXISTS users;
 
 PRAGMA foreign_keys = ON;
 
-.read 'database management/ddl.sql'
-.read 'database management/insert_users.sql'
-.read 'database management/insert_posts.sql'
-.read 'database management/insert_replies.sql'
+.read 'ddl.sql'
+.read 'insert_users.sql'
+.read 'insert_posts.sql'
+.read 'insert_replies.sql'
--- reset_db.ps1
+++
@@ -1,3 +0,0 @@
-Push-Location $PSScriptRoot
-(sqlite3 .\database\cqadb.sqlite ".read 'database management/reset_db.sql'") *> $null
-Pop-Location
