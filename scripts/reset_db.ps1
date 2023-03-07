8a687e155b1f74d8ac0b268d63e8574eaede8bf8
d-chandrahas <chandrahasreddy732@gmail.com>
2023-03-07 13:23:53 +0530

    updated ps script

--- scripts/reset_db.ps1
+++ scripts/reset_db.ps1
@@ -1,5 +1,5 @@
-Push-Location $PSScriptRoot
-Push-Location "../database management"
-(sqlite3 "../database/cqadb.sqlite" ".read 'reset_db.sql'")
-Pop-Location
+Push-Location
+cd $PSScriptRoot
+cd "../database management"
+sqlite3 "../database/cqadb.sqlite" ".read 'reset_db.sql'"
 Pop-Location
